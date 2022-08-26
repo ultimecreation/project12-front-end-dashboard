8edc93fada91db28f2c0d6bf8ff722cc6af34b88
ultimecreation <naserbechata@gmail.com>
2022-08-26 18:57:54 +0200

    completed user health section

---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './ListItem.scss'
+import PropTypes from 'prop-types'
+
+const ListItem = (props) => {
+    console.log(props)
+    return (
+        <div className='list-item'>
+            <figure >
+                <img src={props.img} alt={props.text} />
+            </figure>
+            <ul>
+                <li>
+                    {props.value}
+                </li>
+                <li>
+                    <small>{props.text}</small>
+                </li>
+            </ul>
+        </div>
+
+    )
+}
+
+ListItem.propTypes = {
+    text: PropTypes.string,
+    value: PropTypes.string,
+    img: PropTypes.string
+}
+
+export default ListItem
--- src/components/UserHealth/UserHealth.js
+++ src/components/UserHealth/UserHealth.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import './UserHealth.scss'
+import PropTypes from 'prop-types'
+import ListItem from '../ListItem/ListItem'
 
-const UserHealth = () => {
-  return (
-    <div className='user-health'>
-      user health
-    </div>
-  )
+const UserHealth = (props) => {
+    const items = [
+        {
+            text: 'Calories',
+            value: `${props.keyData.calorieCount}kCal`,
+            img: `${process.env.PUBLIC_URL}/images/calories-icon.png`
+        },
+        {
+            text: 'Protéines',
+            value: `${props.keyData.proteinCount}g`,
+            img: `${process.env.PUBLIC_URL}/images/protein-icon.png`
+        },
+        {
+            text: 'Glucides',
+            value: `${props.keyData.carbohydrateCount}g`,
+            img: `${process.env.PUBLIC_URL}/images/carbs-icon.png`
+        },
+        {
+            text: 'Lipides',
+            value: `${props.keyData.lipidCount}g`,
+            img: `${process.env.PUBLIC_URL}/images/fat-icon.png`
+        }
+    ]
+
+    return (
+        <div className='user-health'>
+            {
+                items.map((item, index) => {
+                    return <ListItem
+                        key={index}
+                        text={item.text}
+                        value={item.value}
+                        img={item.img}
+                    />
+                })
+            }
+        </div>
+    )
 }
 
+UserHealth.propTypes = {
+    keyData: PropTypes.object
+}
 export default UserHealth
